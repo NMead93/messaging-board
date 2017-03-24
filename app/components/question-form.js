dcb712a8093a810cd260890fd05fdae030fb389d
Nicholas Mead <nixter11@uw.edu>
2017-03-24 10:01:25 -0700

    change question form to handlebar inputs

--- app/components/question-form.js
+++ app/components/question-form.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    isFormShowing: false,
+    actions: {
+        formShow: function() {
+            this.set('isFormShowing', true);
+        },
+        formHide: function() {
+            this.set('isFormShowing', false);
+        }
+    }
 });
