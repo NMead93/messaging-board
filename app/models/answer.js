21544417e965804c7d24fa817aecb78d0f09836e
Nicholas Mead <nixter11@uw.edu>
2017-03-24 09:25:51 -0700

    add all questions to index page

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     author: DS.attr(),
     job: DS.attr(),
-    company: DS.attr()
+    company: DS.attr(),
     body: DS.attr(),
     date: DS.attr(),
     question: DS.belongsTo('answer', {async: true})
