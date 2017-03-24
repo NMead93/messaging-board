47116c19a6db6b47de5ba1866b0e6492b6f4d5f5
Nicholas Mead <nixter11@uw.edu>
2017-03-24 10:11:35 -0700

    finish adding questions

--- app/components/question-form.js
+++ app/components/question-form.js
@@ -8,6 +8,22 @@ export default Ember.Component.extend({
         },
         formHide: function() {
             this.set('isFormShowing', false);
+        },
+        saveQuestion() {
+            var params = {
+                title: this.get('title'),
+                body: this.get('body'),
+                author: this.get('author'),
+                date: today()
+            }
+            this.set('isFormShowing', false);
+            this.sendAction('saveQuestion', params)
         }
     }
 });
+
+function today() {
+  var today = new Date();
+  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  return monthNames[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
+}
